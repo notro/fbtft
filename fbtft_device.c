81d984a08986572b78ec84fe86b8861f14766945
notro <notro@tronnes.org>
2013-05-16 21:12:13 +0200

    itdb28fb add SPI support

--- fbtft_device.c
+++ fbtft_device.c
@@ -167,6 +167,17 @@ static struct spi_board_info fbtft_device_spi_displays[] = {
 				{},
 			},
 		}
+	}, {
+		.modalias = "itdb28spifb",
+		.max_speed_hz = 32000000,
+		.mode = SPI_MODE_0,
+		.platform_data = &(struct fbtft_platform_data) {
+			.gpios = (const struct fbtft_gpio []) {
+				{ "reset", 25 },
+				{ "dc", 24 },
+				{},
+			},
+		}
 	}
 };
 

bdfce9f76b7e3e7cc7b9e89ac8e419b160d18adb
notro <notro@tronnes.org>
2014-02-22 18:07:47 +0100

    fbtft_device: add rpi-display entry

--- fbtft_device.c
+++ fbtft_device.c
@@ -526,6 +526,26 @@ static struct fbtft_device_display displays[] = {
 			}
 		}
 	}, {
+		.name = "rpi-display",
+		.spi = &(struct spi_board_info) {
+			.modalias = "fb_ili9341",
+			.max_speed_hz = 32000000,
+			.mode = SPI_MODE_0,
+			.platform_data = &(struct fbtft_platform_data) {
+				.display = {
+					.buswidth = 8,
+					.backlight = 1,
+				},
+				.bgr = true,
+				.gpios = (const struct fbtft_gpio []) {
+					{ "reset", 23 },
+					{ "dc", 24 },
+					{ "led", 18 },
+					{},
+				},
+			}
+		}
+	}, {
 		.name = "sainsmart18",
 		.spi = &(struct spi_board_info) {
 			.modalias = "fb_st7735r",

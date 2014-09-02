32995715c7fb161bf539fefc7e250fef3599cd61
notro <notro@tronnes.org>
2014-09-02 20:14:14 +0200

    fbtft_device: add adafruit28

--- fbtft_device.c
+++ fbtft_device.c
@@ -249,6 +249,26 @@ static struct fbtft_device_display displays[] = {
 			}
 		}
 	}, {
+		.name = "adafruit28",
+		.spi = &(struct spi_board_info) {
+			.modalias = "fb_ili9341",
+			.max_speed_hz = 32000000,
+			.mode = SPI_MODE_0,
+			.platform_data = &(struct fbtft_platform_data) {
+				.display = {
+					.buswidth = 8,
+					.backlight = 1,
+				},
+				.bgr = true,
+				.gpios = (const struct fbtft_gpio []) {
+					{ "reset", 25 },
+					{ "dc", 24 },
+					{ "led", 18 },
+					{},
+				},
+			}
+		}
+	}, {
 		.name = "adafruit13m",
 		.spi = &(struct spi_board_info) {
 			.modalias = "fb_ssd1306",
